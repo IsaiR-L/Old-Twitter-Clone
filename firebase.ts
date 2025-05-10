6f7b5ded082f67d5e11de1610712d8f1a7b0b446
Isai Ramirez-Lopez <isaimiguel.rl@gmail.com>
2025-05-10 08:18:26 +0000

    Changes to support firebase with authentication with new sdk 53 and react and react renderer mismismatch fix

--- firebase.ts
+++ firebase.ts
@@ -1,9 +1,7 @@
-import { initializeApp, getApp, getApps, FirebaseApp } from "firebase/app";
-import { initializeAuth, getAuth, Auth } from "firebase/auth";
-import { getFirestore, Firestore } from "firebase/firestore";
+import { initializeApp, getApp, getApps } from "firebase/app";
+import { initializeAuth, getAuth } from "firebase/auth"; // Auth
+import { getFirestore } from "firebase/firestore"; // Firestore
 import { Platform } from "react-native";
-import AsyncStorage from "@react-native-async-storage/async-storage";
-import { getReactNativePersistence } from "firebase/auth/react-native";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBf4cwe5P7xFpYFiU6BGNYq3EV8892aN_M",
@@ -16,23 +14,10 @@ const firebaseConfig = {
 };
 
 // Initialize the app if it hasn't been initialized yet
-const app: FirebaseApp = getApps().length ? getApp() : initializeApp(firebaseConfig);
+const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
 
-let auth: Auth;
+const auth = initializeAuth(app); // Use memory persistence for React Native
 
-if (Platform.OS === "web") {
-  auth = getAuth(app); // Use default for web
-} else {
-  try {
-    auth = initializeAuth(app, {
-      persistence: getReactNativePersistence(AsyncStorage), // Use React Native persistence
-    });
-  } catch (e) {
-    console.error("Error initializing Firebase Auth:", e);
-    auth = getAuth(app); // Fallback if auth is already initialized
-  }
-}
-
-const db: Firestore = getFirestore(app);
+const db = getFirestore(app);
 
 export { app, auth, db };
